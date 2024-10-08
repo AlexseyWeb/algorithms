7b48ba9c8f48308caff458e99686f17d17b885a5
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-10-08 03:38:22 +0000

    algoritm FreezBuzz

---
+++ freezbuzz.py
@@ -0,0 +1,13 @@
+"""A simple algorithm FreezBuzz """
+
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+for i in list_of_numbers:
+    if i % 15 == 0:
+        print("FreezBuzz")
+    elif i % 5 == 0:
+        printf("Buzz")
+    elif i % 3 == 0:
+        print("Freez")
+
