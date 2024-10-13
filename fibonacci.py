8b551199ba45ec2f1af0a92845d34ffd85e40b0c
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-10-13 12:33:47 +0000

    algoritm Fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+""" Числа Фибоначи """
+
+count_numbers = 15
+first, second = 1, 1
+
+print(first, second, end=" ")
+
+for i in range(count_numbers-2):
+    first, second= second, first + second
+    print(second, end=" ")
+
+print()
