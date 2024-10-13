5fa4b4fa106c2df845d839010155a1a355bc0b01
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-10-13 15:34:49 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -6,7 +6,7 @@ first, second = 1, 1
 print(first, second, end=" ")
 
 for i in range(count_numbers-2):
-    first, second= second, first + second
+    first, second = second, first + second
     print(second, end=" ")
 
 print()
