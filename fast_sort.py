367f084462de7dce94cf9e72f51d3342a9b32ab9
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-10-16 04:31:27 +0000

    added quicksort algoritm

---
+++ fast_sort.py
@@ -0,0 +1,15 @@
+"""Быстрая сортировка """
+
+def quicksort(my_array):
+    if len(my_array) <= 1:
+        return my_array
+    pivot = my_array[len(my_array) // 2]
+    left = [x for x in my_array if x < pivot]
+    middle = [x for x in my_array if  x == pivot]
+    right = [x for x in my_array if x > pivot]
+
+    return quicksort(left) + middle + quicksort(right)
+
+arr = [3, 6, 8, 10, 1, 2, 1]
+
+print(quicksort(arr))
