c5786362af7c740c3f9f68d252e4d429f090295b
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-10-17 17:30:45 +0000

    added method memorization of fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -10,3 +10,14 @@ for i in range(count_numbers-2):
     print(second, end=" ")
 
 print()
+
+#Итеративный динамический подход
+def fibonacci(n):
+    memo = [1, 1]
+
+    for i in range(2, n+1):
+        memo.append(memo[i-1] + memo[i-2])
+        
+    return memo[n]
+
+print(f" Сумма 15-го числа Фибоначи => {fibonacci(15)}")
