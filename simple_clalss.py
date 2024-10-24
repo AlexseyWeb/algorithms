9f8c76309e3250879f2e36dfd770e8cfbf42b148
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-10-24 03:56:37 +0000

    update

--- simple_clalss.py
+++ simple_clalss.py
@@ -6,5 +6,19 @@ class Person:
     def output(self):
         print(f"{self.name} -> {self.age}")
 
+
+
+class Programmer(Person):
+    def __init__(self, language=None, sallary=10000):
+        self.language = language
+        self.sallary = sallary
+
+    def info(self):
+        print( f"{self.language} -> {self.sallary}")
+
+
 alex = Person("Alex", 33)
 alex.output()
+
+web_developer = Programmer("Python", 120000)
+web_developer.info()
