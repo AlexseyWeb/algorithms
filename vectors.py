b621b7c6091728149d84eedd3575b8277e9a5548
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-11-12 04:41:01 +0000

    update

--- vectors.py
+++ vectors.py
@@ -1,7 +1,11 @@
-def add_vectors(v1, v2):
-    return (v1[0] + v2[0], v1[1] + v2[1])
+class Vector:
+    def add_vectors(self, v1, v2):
+        return (v1[0] + v2[0], v1[1] + v2[1])
 
 first_vector = (3, 4)
 second_vector = (-1, 2)
 
-print(add_vectors(first_vector, second_vector))
+vec = Vector()
+print(vec.add_vectors(first_vector, second_vector))    
+
+
