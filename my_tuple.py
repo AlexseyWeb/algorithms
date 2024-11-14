5419696ce70b29495ed29924c31caea158e94dc0
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-11-14 05:38:43 +0000

    update

---
+++ my_tuple.py
@@ -0,0 +1,12 @@
+"""Work with tuple"""
+
+apple = 'apple'
+banana = 'banana'
+lime = 'lime'
+
+fruits = (apple, banana, lime)
+
+number_fruits = (1, 2, 3 ).__add__(fruits)
+
+print(fruits)
+print(number_fruits)
