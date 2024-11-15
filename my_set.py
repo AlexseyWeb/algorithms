c329946aedee6c53e9ea9a3f65badb0bf6ac51ef
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-11-15 04:26:48 +0000

    update

---
+++ my_set.py
@@ -0,0 +1,19 @@
+""" Working with set """
+
+#Empty set
+my_set = set()
+
+numbers_set = {3, 22, 11, 88, 99}
+numbers_set.add(101)
+numbers_set.discard(3)
+
+second_set = {3, 88, 111, 222, 9999}
+
+print(f"Union sets ->  {numbers_set | second_set}")\
+
+
+print(my_set)
+print(numbers_set)
+
+
+
