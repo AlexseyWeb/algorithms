d893260e4128985d2d62b3b27cb74436e20bbfa4
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-11-16 05:32:09 +0000

    updated

--- mutable.py
+++ mutable.py
@@ -6,3 +6,10 @@ other_list = number_list
 other_list.append(9)
 
 print(f"Mutable object created everytime {number_list} -> {other_list} ")
+
+""" To avoid changing objects, you need to use the copy method """
+
+another_list = number_list.copy()
+another_list.append(100)
+
+print(f"{number_list} -> {another_list}")
