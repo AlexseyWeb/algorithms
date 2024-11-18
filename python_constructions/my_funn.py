7899673e0f84dfddecce973744310daf5fc74bb6
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2024-11-18 07:33:05 +0000

    updated functions

--- python_constructions/my_funn.py
+++ python_constructions/my_funn.py
@@ -19,3 +19,27 @@ def greeting(greet):
 
 x = greeting("Hello ")
 print(x("Alexsey"))
+
+def dict_to_list(dict_to_convert):
+    list_for_convertion = []
+    for key, value in dict_to_convert.items():
+        if type(value) == int:
+            value *= 2
+        list_for_convertion.append((key, value))
+    return list_for_convertion
+
+
+print(dict_to_list({'a': 3, 'b': [], 'c': 100}))
+
+
+def fillter_list(list_to_fillter, value_type):
+    filtered_list = []
+    for element in list_to_fillter:
+        if type(element) == value_type:
+            filtered_list.append(element)
+    return filtered_list
+
+
+print(fillter_list([35, True, 'abc', 10], int))
+print(fillter_list([35, True, 'abc', 10], str))
+print(fillter_list([35, True, 'abc', 10], bool))
