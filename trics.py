7dcb5f965509b2926796e9e2aca16478804072cc
AlexseyWeb <alongenet32@gmail.com>
2024-12-16 06:52:40 +0300

    added  93 trick

--- trics.py
+++ trics.py
@@ -1303,13 +1303,59 @@ with open('test.txt', 'r', encoding='utf-8') as file:
         print(line.strip())
 
 # 92. input filename to read and handle exceptions
-fnmae = input('Enter the file name: ')
-try:
-    fhand = open(fnmae)
-except:
-    print('File cannot be opened: ', fnmae)
-count = 0
-for line in fhand:
-    if line.startswith('Subject:')
-    count = count + 1
-print('There were', count, 'subject lines in', fnmae)
+
+
+def filename_read():
+    fnmae = input('Enter the file name: ')
+    try:
+        fhand = open(fnmae)
+    except:
+        print('File cannot be opened: ', fnmae)
+    count = 0
+    for line in fhand:
+        if line.startswith('Subject:'):
+            count = count + 1
+    print('There were', count, 'subject lines in', fnmae)
+
+# 93. Stack with list
+
+
+class MyStack:
+    def __init__(self):
+        self.array = []
+
+    def push(self, item):
+        self.array.append(item)
+
+    def pop(self):
+        popped_item = self.array.pop()
+        return popped_item
+
+    def peek(self):
+        return self.__current()
+
+    def __current(self):
+        return self.array[self.count()-1]
+
+    def count(self):
+        return len(self.array)
+
+    def __iter__(self):
+        self.index = self.count()-1
+        return self
+
+    def __next__(self):
+        if self.index < 0:
+            raise StopIteration()
+        result = self.array[self.index]
+        self.index -= 1
+        return result
+
+
+stack = MyStack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())
+print(stack.peek())
+print(stack.count())
