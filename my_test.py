19fff49fa5d2ccbc6aa60156fc7e9aa50bda0c4a
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2025-02-19 06:25:22 +0300

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -12,6 +12,9 @@ class MyAddTestSuite(unittest.TestCase):
     def test_add3(self):
         self.assertEqual(-15, add(-10, -5), "should be -15")
 
+    def test_add4(self):
+        self.assertEqual(4, add(1, 3), "should be 4")
+
 
 if __name__ == "__main__":
     unittest.main()
